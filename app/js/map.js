64cf3508c3f961039f3c74f95f12b78a23e1f88f
Zhurin Vladislav <zhurinvladislavweb@gmail.com>
2022-11-12 01:00:19 +0300

    mod map

--- app/js/map.js
+++ app/js/map.js
@@ -21,4 +21,43 @@ document.addEventListener('DOMContentLoaded', function() {
       body.classList.remove("noscroll-map");
     }
   }
-})
+
+  ymaps.ready(init);
+  function init() {
+  const mapElem = document.querySelector('#map');
+  const myMap = new ymaps.Map(
+    "map",
+    { 
+    center: [55.75846806898367, 37.60108849999989], 
+    zoom: 14, 
+    controls: ['geolocationControl', 'zoomControl']
+    },
+    { 
+    suppressMapOpenBlock: true,
+    geolocationControlSize: "large",
+    geolocationControlPosition:  { top: "400px", right: "20px" },
+    geolocationControlFloat: 'none',
+    zoomControlSize: "small",
+    zoomControlFloat: "none",
+    zoomControlPosition: { top: "320px", right: "20px" }
+    }
+  );
+
+  myMap.behaviors.disable('scrollZoom');
+
+  const myPlacemark = new ymaps.Placemark(
+    [55.75846806898367, 37.60108849999989],
+    {},
+    {
+    iconLayout: 'default#image',
+    iconImageHref: 'img/subtract.svg',
+    iconImageSize: [20, 20],
+    iconImageOffset: [-3, 42]
+    }
+  );
+
+  myMap.geoObjects.add(myPlacemark);
+  myMap.container.fitToViewport();
+  }
+});
+
