905051ef36776dafc800e81a33f0b0b81e3f3553
Zhurin Vladislav <zhurinvladislavweb@gmail.com>
2022-11-15 23:24:11 +0300

    add text dop

--- app/js/map.js
+++ app/js/map.js
@@ -1,11 +1,15 @@
 document.addEventListener("DOMContentLoaded", function() {
   const btnClick = document.querySelector('#btn-text');
-  const btnText = document.querySelector('.text');
+  const btnText = document.querySelector('#text-addition');
+  const btnClose = document.querySelector('#text-close');
 
-  btnClick.addEventListener("click", addText());
+  btnClick.addEventListener("click", function() {
+    btnText.classList.add("active");
+    btnClick.classList.add("disabled");
+  });
 
-  function addText() {
-    btnText.classList.toggle("active");
-    console.log("Нажал на кнопку")
-  }
+  btnClose.addEventListener("click", function() {
+    btnText.classList.remove("active");
+    btnClick.classList.remove("disabled");
+  })
 })
